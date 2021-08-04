bb25c27bcc486e2a01ea0b9f10ce5fcd5c052f44
saba989 <saba17510@gmail.com>
2021-08-04 22:40:20 +0530

    assignment

---
+++ Week2/Day2/src/ExceptionHandling/InvalidAadharException.java
@@ -0,0 +1,15 @@
+package ExceptionHandling;
+
+public class InvalidAadharException extends Exception {
+
+	public InvalidAadharException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidAadharException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
